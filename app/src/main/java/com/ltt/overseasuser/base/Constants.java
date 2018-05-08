88cf174270d5ce89f061b4f00f0075191d03bc58
xyzkissyou <505763305@qq.com>
2018-05-08 09:08:42 +0800

    201805080908

--- app/src/main/java/com/ltt/overseasuser/base/Constants.java
+++ app/src/main/java/com/ltt/overseasuser/base/Constants.java
@@ -4,5 +4,5 @@ import java.io.Serializable;
 
 public class Constants {
 
-    public static final String BASE_URL = "https://popmach.com/api/";
+    public static final String BASE_URL = "https://dev.popmach.com/api/";
 }
