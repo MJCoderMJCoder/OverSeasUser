8a94b0c37a7ff8186ad6e228114f2b07f170a63c
xyzkissyou <505763305@qq.com>
2018-05-08 10:21:29 +0800

    地址更改为开发用的地址

--- app/src/main/java/com/ltt/overseasuser/base/Constants.java
+++ app/src/main/java/com/ltt/overseasuser/base/Constants.java
@@ -2,6 +2,9 @@ package com.ltt.overseasuser.base;
 
 import java.io.Serializable;
 
+/**
+ * 地址更改为开发地址
+ */
 public class Constants {
 
     public static final String BASE_URL = "https://dev.popmach.com/api/";
