0341ca0f2b7779f0cad30ac86cecece8f289fe0d
xyzkissyou <505763305@qq.com>
2018-05-12 14:55:22 +0800

    LoginBean

---
+++ app/src/main/java/com/ltt/overseasuser/model/LoginBean.java
@@ -0,0 +1,27 @@
+package com.ltt.overseasuser.model;
+
+/**
+ * Created by Administrator on 2018/5/12.
+ */
+
+public class LoginBean {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String email;
+    private String password;
+
+}
