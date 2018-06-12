bf067f1f80297eb6e964be2457f3216497b056f0
xyzkissyou <505763305@qq.com>
2018-06-12 11:13:46 +0800

    add facebook

--- app/src/main/java/com/ltt/overseasuser/login/LoginActivity.java
+++ app/src/main/java/com/ltt/overseasuser/login/LoginActivity.java
@@ -3,7 +3,16 @@ package com.ltt.overseasuser.login;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import com.ltt.overseasuser.MainActivity;
 import com.ltt.overseasuser.R;
@@ -19,6 +28,8 @@ import com.ltt.overseasuser.model.UserBean;
 import com.ltt.overseasuser.utils.PreferencesUtils;
 import com.ltt.overseasuser.utils.ToastUtils;
 
+import java.util.Arrays;
+
 import butterknife.BindView;
 import butterknife.OnClick;
 import retrofit2.Call;
@@ -27,6 +38,9 @@ import retrofit2.Call;
  * Created by Administrator on 2018/1/17.
  */
 public class LoginActivity extends BaseActivity {
+    private static final String EMAIL = "email";
+    private static final String USER_POSTS = "user_posts";
+    private CallbackManager mCallbackManager;
     ActionBar bar;
     @BindView(R.id.et_email)
     EditText etEmail;
@@ -35,6 +49,7 @@ public class LoginActivity extends BaseActivity {
 
     private String mEmail, mPwd;
 
+    private LoginButton facebookButton;
     @Override
     protected int bindLayoutID() {
         return R.layout.activity_login;
@@ -50,22 +65,54 @@ public class LoginActivity extends BaseActivity {
             }
         });
         bar.setTitle("LOG IN");
+        mCallbackManager = CallbackManager.Factory.create();
+
+        facebookButton=(LoginButton)findViewById(R.id.btn_fblogin) ;
+        // Set the initial permissions to request from the user while logging in
+        facebookButton.setReadPermissions(Arrays.asList(EMAIL, USER_POSTS));
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AccessToken accessToken = AccessToken.getCurrentAccessToken();
+                boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+                if(isLoggedIn){
+                    loginWithFacebook(accessToken.getToken());
+                }
+            }
+        });
+        // Register a callback to respond to the user
+        facebookButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                setResult(RESULT_OK);
+
+                finish();
+            }
+            @Override
+            public void onCancel() {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+            @Override
+            public void onError(FacebookException e) {
+                // Handle exception
+                Log.d("onError:",e.toString());
+            }
+        });
     }
 
-    @OnClick({R.id.tv_forget_pwd, R.id.iv_fblogin, R.id.iv_googlelogin, R.id.btn_login})
+    @OnClick({R.id.tv_forget_pwd, R.id.iv_googlelogin, R.id.btn_login})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tv_forget_pwd:
                 startActivity(new Intent(this, ForgetActivity.class));
                 break;
-            case R.id.iv_fblogin:
-                ToastUtils.showToast("facebook login");
-                break;
             case R.id.iv_googlelogin:
                 ToastUtils.showToast("google+ login");
                 break;
             case R.id.btn_login:
-              //  startActivity(new Intent(this, MainActivity.class));
+                //  startActivity(new Intent(this, MainActivity.class));
                 if(judgeInput()){
                     login();
                 }
@@ -136,5 +183,42 @@ public class LoginActivity extends BaseActivity {
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        Log.d("onActivityResult:",requestCode+""+resultCode);
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // Log in with facebook
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        Log.d("accessToken:",""+accessToken.getToken());
+        String token=accessToken.getToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        if(isLoggedIn)
+            loginWithFacebook(token);
+    }
+
+    private void loginWithFacebook(String token){
+        Call<GsonUserBean> fbcallback = RetrofitUtil.getAPIService().callback(token);
+        fbcallback.enqueue(new CustomerCallBack<GsonUserBean>() {
+            @Override
+            public void onResponseResult(GsonUserBean response) {
+                Log.d("login success....:",""+response.getCode());
+                UserBean userBean=response.getData();
+                if(null==userBean)
+                    userBean=new UserBean();
+                XApplication.globalUserBean = userBean;
+                XApplication.globalUserBean.setAccess_token(response.getAccess_token());
+                PreferencesUtils.saveUserInfoPreference(XApplication.globalUserBean);
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+            }
+
+            @Override
+            public void onResponseError(BaseBean errorMsg, boolean isNetError) {
+                // dismissLoadingView();
+                //Log.d("onResponseError",errorMsg.getMsg());
+            }
+        });
+    }
 }
